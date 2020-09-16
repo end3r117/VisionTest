a675476fa2b271028eab9652396adfd47f5e7d30
Anthony Rosario <anthonymrosario@gmail.com>
2020-09-15 22:46:35 -0700

    Initial Commit

---
+++ VisionTest/VisionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionTestApp.swift
+//  VisionTest
+//
+//  Created by Anthony Rosario on 9/15/20.
+//
+
+import SwiftUI
+
+@main
+struct VisionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
